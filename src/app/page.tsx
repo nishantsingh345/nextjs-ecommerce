202ec3604c8960b8b8da7fabdec2419d6e45de32
Nishant <nishantsingh95340@gmail.com>
2023-07-20 17:04:11 +0530

    adding produc page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import ProductCard from '@/components/ProductCard'
 import { prisma } from '@/lib/db/prisma'
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default async function Home() {
   const productas = await prisma.producta.findMany({
@@ -7,7 +9,30 @@ export default async function Home() {
 });
   return (
    <div>
-      <ProductCard producta={productas[0]}/>
+     <div className="hero rounded-xl bg-base-200">
+       <div className='hero-content flex-col lg:flex-row'>
+          <Image 
+          src={productas[0].imageUrl}
+          alt={productas[0].name}
+          width={400}
+          height={800}
+          className='w-full max-w-sm rounded-lg shadow-2xl'
+          priority
+          />
+          <div>
+            <h1 className='text-5xl font-bold'>{productas[0].name}</h1>
+            <p className='py-6'>{productas[0].description}</p>
+            <Link href={"/productas/" + productas[0].id} className='btn btn-primary'>
+              Check it Out
+            </Link>
+          </div>
+      </div>
+     </div>
+     <div className="my-4 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+      {productas.slice(1).map(producta => (
+        <ProductCard producta={producta} key={producta.id}/>
+      ))}
+     </div>
    </div>
   )
 }
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { Producta } from "@prisma/client"
 import Link from "next/link"
 import PriceTag from "./PriceTag"
+import Image from "next/image"
 
 
 interface ProductCardProps {
@@ -8,10 +9,21 @@ interface ProductCardProps {
 }
 
 export default function ProductCard({producta}:ProductCardProps){
-    return(
+    const isnew = Date.now() - new Date(producta.createdAt).getTime() < 1000 * 60 *60 * 24 * 7 ;
+        return(
            <Link href={"/productas/" + producta.id}  className="card w-full bg-base-100 hover:shadow-xl transition-shadow">
+           <figure>
+            <Image 
+            src={producta.imageUrl}
+            alt={producta.name}
+            width={800}
+            height={400}
+            className="h-48 object-cover "
+            />
+           </figure>
             <div className="card-body">
                 <h2 className="card-title ">{producta.name}</h2>
+                 {isnew && <div className="badge badge-secondary">NEW</div>}
                 <p className="">{producta.description}</p>
                 <PriceTag price={producta.price}/>
             </div>
